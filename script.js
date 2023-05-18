457a3c0268c075395312d6122e1145e5ca047027
Marco Mercado <marco@stonepawtraining.com>
2023-05-18 17:09:06 -0400

    Added the playRound function

--- script.js
+++ script.js
@@ -10,7 +10,55 @@ getComputerChoice = function () {
   }
 };
 
-const compAnswer = getComputerChoice();
+let compPts = 0;
+let playerPts = 0;
 
-console.log(compAnswer);
+playRound = function (playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
 
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    return compPts++;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    return playerPts++;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    return playerPts++;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    return compPts++;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    return compPts++;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    return playerPts++;
+  } else {
+    console.log(`It's a draw.`);
+  }
+};
+
+game = function () {
+  for (let i = 0; i <= 4; i++) {
+    const compAnswer = getComputerChoice();
+    const playerAnswer = prompt(
+      'Please type, "rock", "paper", or "scissors" to choose your hand'
+    );
+
+    playRound(playerAnswer, compAnswer);
+  }
+
+  if (playerPts > compPts) {
+    console.log(`You beat the computer ${playerPts} to ${compPts}`);
+  } else {
+    console.log(`You lost to the computer ${compPts} to ${playerPts}`);
+  }
+};
+
+game();
+/**
+ *
+ *
+ */
