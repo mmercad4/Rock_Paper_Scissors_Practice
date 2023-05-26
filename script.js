b22eca38a32bce627e092a622c5c4b2797a88a93
Marco Mercado <marco@stonepawtraining.com>
2023-05-25 20:47:31 -0400

    Added UI to the game

--- script.js
+++ script.js
@@ -60,6 +60,72 @@ game = function () {
     console.log(`You lost to the computer ${compPts} to ${playerPts}`);
   }
 };
+
+const buttonContainer = document.querySelector(".button-container");
+
+const buttons = buttonContainer.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const compAnswer = getComputerChoice().toLowerCase();
+    const playerAnswer = e.target.textContent.toLowerCase();
+
+    playRound(playerAnswer, compAnswer);
+
+    const userHead = document.querySelector(".userHead");
+    const cpuHead = document.querySelector(".cpuHead");
+    const winnerHead = document.querySelector(".winner");
+    const playerImg = document.querySelector(".playerImg");
+    const cpuImg = document.querySelector(".cpuImg");
+
+    drawImg =
+      "https://cdn.pixabay.com/photo/2013/07/12/16/35/redo-151210_1280.png";
+
+    scissorImg =
+      "https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296853_1280.png";
+
+    paperImg =
+      "https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296855_1280.png";
+
+    rockImg =
+      "https://cdn.pixabay.com/photo/2014/03/25/15/26/rock-paper-scissors-296854_1280.png";
+
+    if (playerAnswer === "rock" && compAnswer === "paper") {
+      playerImg.src = rockImg;
+      cpuImg.src = paperImg;
+    } else if (playerAnswer === "rock" && compAnswer === "scissors") {
+      playerImg.src = rockImg;
+      cpuImg.src = scissorImg;
+    } else if (playerAnswer === "paper" && compAnswer === "rock") {
+      playerImg.src = paperImg;
+      cpuImg.src = rockImg;
+    } else if (playerAnswer === "paper" && compAnswer === "scissors") {
+      playerImg.src = paperImg;
+      cpuImg.src = scissorImg;
+    } else if (playerAnswer === "scissors" && compAnswer === "rock") {
+      playerImg.src = scissorImg;
+      cpuImg.src = rockImg;
+    } else if (playerAnswer === "scissors" && compAnswer === "paper") {
+      playerImg.src = scissorImg;
+      cpuImg.src = paperImg;
+    } else if (playerAnswer === compAnswer) {
+      playerImg.src = drawImg;
+      cpuImg.src = drawImg;
+    }
+
+    userHead.textContent = `User: ${playerPts}`;
+    cpuHead.textContent = `CPU: ${compPts}`;
+
+    if (compPts == 5) {
+      winnerHead.textContent = `You lose!`;
+      winnerHead.classList.add("red");
+    } else if (playerPts == 5) {
+      winnerHead.textContent = "You win!";
+      winnerHead.classList.add("green");
+    }
+  });
+});
+
 /**
  *
  *
